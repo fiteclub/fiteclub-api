c899dd9cfd15289e669dd9cffd843b398fe1dd09
fiteclub <mattf255@yahoo.com>
2021-06-24 18:02:56 -0400

    Create has_many associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   belongs_to :resume
+  has_many :list_items
 end
--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,2 +1,4 @@
 class Resume < ApplicationRecord
+  has_many :categories
+  has_many :list_items, through: :categories
 end
