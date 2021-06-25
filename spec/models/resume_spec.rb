70eba768d0917623ca7b837afbf40d7a6f0f9414
fiteclub <mattf255@yahoo.com>
2021-06-25 17:44:01 -0400

    Refactor model specs with shoulda-matchers

--- spec/models/resume_spec.rb
+++ spec/models/resume_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Resume, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:categories) }
+    it { should have_many(:list_items) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
 end
