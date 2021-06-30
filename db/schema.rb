b634f68b1fe44ff96cb3c03f8faca060e1f63ae9
fiteclub <mattf255@yahoo.com>
2021-06-29 22:50:36 -0400

    Generate Education model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_014555) do
+ActiveRecord::Schema.define(version: 2021_06_30_025018) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(version: 2021_06_30_014555) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["resume_id"], name: "index_categories_on_resume_id"
+  end
+
+  create_table "educations", force: :cascade do |t|
+    t.bigint "resume_id", null: false
+    t.string "organization"
+    t.string "degree"
+    t.integer "graduate_year"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["resume_id"], name: "index_educations_on_resume_id"
   end
 
   create_table "jobs", force: :cascade do |t|
@@ -51,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_06_30_014555) do
   end
 
   add_foreign_key "categories", "resumes"
+  add_foreign_key "educations", "resumes"
   add_foreign_key "jobs", "resumes"
   add_foreign_key "list_items", "categories"
 end
---
+++ spec/factories/educations.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :education do
+    resume { nil }
+    organization { "MyString" }
+    degree { "MyString" }
+    grad_year { "" }
+  end
+end
