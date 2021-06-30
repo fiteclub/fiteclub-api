c77bc146748b0a9c3a410cdaec73b234e2c7b0c5
fiteclub <mattf255@yahoo.com>
2021-06-29 22:13:46 -0400

    Create Job factory and spec

--- spec/factories/jobs.rb
+++ spec/factories/jobs.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :job do
-    resume { nil }
-    description { "MyString" }
+    association :resume
+    organization { "Pawnee Parks LLC" }
+    title { "Director" }
+    start_year { 1990 }
+    end_year { 2020 }
   end
 end
