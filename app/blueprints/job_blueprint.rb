ecf9f97b641f90f3a5060809dab2f43b2615df94
fiteclub <mattf255@yahoo.com>
2021-07-09 16:17:47 -0400

    Add list_items to job blueprint

--- app/blueprints/job_blueprint.rb
+++ app/blueprints/job_blueprint.rb
@@ -1,5 +1,5 @@
 class JobBlueprint < Blueprinter::Base
   identifier :organization
   fields :title, :start_year, :end_year
-  # association :list_items, blueprint: ListItemBlueprint
+  association :list_items, blueprint: ListItemBlueprint
 end
